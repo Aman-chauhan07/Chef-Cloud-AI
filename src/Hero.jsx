c69abbb5ecc9b1d308c149b28205c4fb09155dc7
Aman-chauhan07 <amanx1class@gmail.com>
2025-08-18 16:15:20 +0530

    Hero add

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,1 +1,12 @@
-afdsfsd
+import "./Hero.css";
+
+function Hero(){
+
+    return <form>
+        <input type="text" placeholder="e.g. orgeneo" className="input" />
+        <button className="btn">Add Ingredient</button>
+    </form>
+
+}
+
+export default  Hero 
