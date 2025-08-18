70892ea5381e056942fa7a8317879980531a36da
Aman-chauhan07 <amanx1class@gmail.com>
2025-08-18 19:08:55 +0530

    #commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./Hero.css";
 
 function Hero(){
-
+    
     return <form>
         <input type="text" placeholder="e.g. orgeneo" className="input" />
         <button className="btn">Add Ingredient</button>
