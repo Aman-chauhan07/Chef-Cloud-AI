7d6be3c76b993b2934cb270f3914087250bf01bf
Aman-chauhan07 <amanx1class@gmail.com>
2025-08-18 14:22:37 +0530

    changments

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../images/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" className="logo" />
+      <h3 className="logo-text">Chef Claude</h3>
+    </div>
+  );
+};
+
+export default Header;
