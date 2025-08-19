fb121cda595598a808a76426c190b007a66b93bf
Aman-chauhan07 <amanx1class@gmail.com>
2025-08-19 09:04:48 +0530

    #commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 import "./Hero.css";
 
-function Hero(){
-    return <form>
-        <input type="text" placeholder="e.g. orgeneo" className="input" />
-        <button className="btn">Add Ingredient</button>
-    </form>
+function Hero() {
+  const [ingredients, setIngredients] = useState([]);
+  const [value, setValue] = useState("");
 
+  const ingredientsListItems = ingredients.map((ingredient, index) => (
+      <li key={index}>{ingredient}</li>
+    ));
+  
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (value.trim() === "") return;
+    setIngredients([...ingredients, value]);
+    setValue("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="btn">Add Ingredient</button>
+      </form>
+      <ul>{ingredientsListItems}</ul>
+    </div>
+  );
 }
 
-export default  Hero 
+export default Hero;
